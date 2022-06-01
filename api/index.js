c221a8bdf04e7f64842c96b4e2690044fa87d3c1
Christopher Gauffin <christopher.gauffin@gmail.com>
2022-06-01 13:28:23 +0200

    mitigation branch

--- api/index.js
+++ api/index.js
@@ -1,22 +1,35 @@
 // Gets express router
+const xss = require("xss");
 const express = require("express");
 const router = express.Router();
 const path = require("path");
 
 let latestPost = null;
 
+const myxss = new xss.FilterXSS({
+    whiteList: {
+        h1: [],
+        span: [],
+        b: [],
+    },
+});
+
 router.post("/create", (req, res) => {
-  const data = req.body.data;
-  latestPost = JSON.parse(data);
-  return res.status(200).send();
+    const data = JSON.parse(req.body.data);
+    console.log(data);
+    const filtered = {title: myxss.process(data.title), body: myxss.process(data.body)};
+    console.log(filtered);
+    latestPost = filtered;
+
+    return res.status(200).send();
 });
 
 router.get("/latest", (req, res) => {
-  return res.status(200).json(latestPost);
+    return res.status(200).json(latestPost);
 });
 
 router.get("/", (req, res) => {
-  console.log("hello");
+    console.log("hello");
 });
 
 module.exports = router;
--- public/js/create.js
+++ public/js/create.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  document.querySelector("#create-form").onsubmit = function (e) {
-    e.preventDefault();
+    document.querySelector("#create-form").onsubmit = function (e) {
+        e.preventDefault();
 
-    let title = document.querySelector("#create-title").value;
-    let body = document.querySelector("#create-body").value;
+        let title = document.querySelector("#create-title").value;
+        let body = document.querySelector("#create-body").value;
 
-    const data = { title: filterXSS(title), body: filterXSS(body) };
+        const data = {title, body};
 
-    fetch("/api/create", {
-      method: "POST",
-      body: "data=" + encodeURIComponent(JSON.stringify(data)),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      },
-    })
-      .then((res) => {
-        if (res.status == 200) {
-          window.location.href = "/blog.html";
-        }
-      })
-      .then((html) => console.log(html))
-      .catch((err) => console.error(err));
-  };
+        fetch("/api/create", {
+            method: "POST",
+            body: "data=" + encodeURIComponent(JSON.stringify(data)),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            },
+        })
+            .then((res) => {
+                if (res.status == 200) {
+                    window.location.href = "/blog.html";
+                }
+            })
+            .then((html) => console.log(html))
+            .catch((err) => console.error(err));
+    };
 });
